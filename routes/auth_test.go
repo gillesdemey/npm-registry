4bf3800d239fe3d3fd34ff6062a84e284342f1f9
Gilles De Mey <gilles.de.mey@gmail.com>
2016-11-23 23:25:53 +0100

    Adds whoami test

---
+++ routes/auth_test.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+  "github.com/urfave/negroni"
+  "github.com/stretchr/testify/assert"
+  "golang.org/x/net/context"
+  "net/http"
+  "net/http/httptest"
+  "testing"
+)
+
+func TestWhoami(t *testing.T) {
+  req, err := http.NewRequest("GET", "/-/whoami", nil)
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  rec := httptest.NewRecorder()
+  n := negroni.New(negroni.HandlerFunc(Whoami))
+
+  ctx := NewRendererContext()
+  ctx = context.WithValue(ctx, "user", "foo")
+  n.ServeHTTP(rec, req.WithContext(ctx))
+
+  assert.Equal(t, rec.Code, http.StatusOK)
+  assert.Equal(t, rec.Body.String(), `{"username":"foo"}`)
+}
