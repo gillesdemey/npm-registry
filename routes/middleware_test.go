cde70639ba8fd0ffd517bcbab44290988590991c
Gilles De Mey <gilles.de.mey@gmail.com>
2016-11-24 00:46:51 +0100

    Adds negative test case for ValidateToken middleware

--- routes/middleware_test.go
+++ routes/middleware_test.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"golang.org/x/net/context"
@@ -30,6 +31,31 @@ func TestValidateToken(t *testing.T) {
 		assert.Equal(t, req.Context().Value("user").(string), "foo")
 		assert.Equal(t, req.Context().Value("token").(string), "abc123")
 	})
+}
+
+func TestValidateTokenWithInvalidToken(t *testing.T) {
+	req, err := http.NewRequest("GET", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("Authorization", "Bearer abc123")
+
+	rec := httptest.NewRecorder()
+
+	ctx := req.Context()
+	storage := new(MockStorage)
+
+	storage.
+		On("RetrieveUsernameFromToken", "abc123").
+		Return("", errors.New("oh no!"))
+
+	ctx = context.WithValue(ctx, "storage", storage)
+
+	ValidateToken(rec, req.WithContext(ctx), func(w http.ResponseWriter, req *http.Request) {
+		t.Fail()
+	})
+
+	assert.Equal(t, rec.Code, http.StatusUnauthorized)
 }
 
 type MockStorage struct {
