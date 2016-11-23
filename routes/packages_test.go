b15bc5184843740748d2eab93aa728f78ced4f61
Gilles De Mey <gilles.de.mey@gmail.com>
2016-11-24 00:21:00 +0100

    adds test for queryEscape

---
+++ routes/packages_test.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+  "github.com/stretchr/testify/assert"
+  "testing"
+)
+
+func TestQueryEscape(t *testing.T) {
+  expected := "@foo%2Fbar"
+  output := queryEscape("@foo/bar")
+  assert.Equal(t, output, expected)
+}
