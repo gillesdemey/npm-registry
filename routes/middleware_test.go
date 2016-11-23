d9b0b27da9bc7fa20e02de1d8892c6f354d4cc18
Gilles De Mey <gilles.de.mey@gmail.com>
2016-11-24 00:36:27 +0100

    Adds simple middleware test

---
+++ routes/middleware_test.go
@@ -0,0 +1,42 @@
+package routes
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"golang.org/x/net/context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestValidateToken(t *testing.T) {
+	req, err := http.NewRequest("GET", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("Authorization", "Bearer abc123")
+
+	rec := httptest.NewRecorder()
+
+	ctx := req.Context()
+	storage := new(MockStorage)
+
+	storage.On("RetrieveUsernameFromToken", "abc123").Return("foo", nil)
+
+	ctx = context.WithValue(ctx, "storage", storage)
+
+	ValidateToken(rec, req.WithContext(ctx), func(w http.ResponseWriter, req *http.Request) {
+		assert.Equal(t, rec.Code, http.StatusOK)
+		assert.Equal(t, req.Context().Value("user").(string), "foo")
+		assert.Equal(t, req.Context().Value("token").(string), "abc123")
+	})
+}
+
+type MockStorage struct {
+	mock.Mock
+}
+
+func (s *MockStorage) RetrieveUsernameFromToken(token string) (string, error) {
+	args := s.Called(token)
+	return args.String(0), args.Error(1)
+}
