b038302b760a348496532e266dd14f678deb6609
Gilles De Mey <gilles.de.mey@gmail.com>
2016-11-24 17:37:21 +0100

    Refacored auth_test.go and adds BadRequest test

--- routes/auth_test.go
+++ routes/auth_test.go
@@ -12,53 +12,49 @@ import (
 )
 
 func TestLogin(t *testing.T) {
-	req, err := http.NewRequest(
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest(
 		"PUT", "/-/user/foo",
 		strings.NewReader(`{"name":"foo","password":"bar"}`),
 	)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rec := httptest.NewRecorder()
-	n := negroni.New()
-	n.UseHandlerFunc(Login)
-
-	storage := new(MockStorage)
-	auth := auth.NewHtpasswdProvider("../test/htpasswd.test")
-
-	ctx := NewRendererContext()
-	ctx = context.WithValue(ctx, "storage", storage)
-	ctx = context.WithValue(ctx, "auth", auth)
-
-	n.ServeHTTP(rec, req.WithContext(ctx))
+	serveTestRequest(rec, req)
 
 	assert.Equal(t, rec.Code, http.StatusCreated)
 	assert.Contains(t, rec.Body.String(), `{"token":"`, `"}`)
 }
 
 func TestLoginInvalid(t *testing.T) {
-	req, err := http.NewRequest(
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest(
 		"PUT", "/-/user/bar",
 		strings.NewReader(`{"name":"bar","password":"foo"}`),
 	)
-	if err != nil {
-		t.Fatal(err)
-	}
+	serveTestRequest(rec, req)
 
+	assert.Equal(t, rec.Code, http.StatusUnauthorized)
+	assert.JSONEq(t, rec.Body.String(), `{"error":"invalid credentials"}`)
+}
+
+func TestLoginBadRequest(t *testing.T) {
 	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest("PUT", "/-/user/foo", strings.NewReader(``))
+	serveTestRequest(rec, req)
+
+	assert.Equal(t, rec.Code, http.StatusBadRequest)
+	assert.JSONEq(t, rec.Body.String(), `{"error":"bad request"}`)
+}
+
+func serveTestRequest(rec *httptest.ResponseRecorder, req *http.Request) {
 	n := negroni.New()
 	n.UseHandlerFunc(Login)
 
 	auth := auth.NewHtpasswdProvider("../test/htpasswd.test")
+	storage := new(MockStorage)
 
 	ctx := NewRendererContext()
+	ctx = context.WithValue(ctx, "storage", storage)
 	ctx = context.WithValue(ctx, "auth", auth)
-
 	n.ServeHTTP(rec, req.WithContext(ctx))
-
-	assert.Equal(t, rec.Code, http.StatusUnauthorized)
-	assert.JSONEq(t, rec.Body.String(), `{"error":"invalid credentials"}`)
 }
 
 func TestWhoami(t *testing.T) {
